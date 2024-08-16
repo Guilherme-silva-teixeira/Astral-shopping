99f851fbaf50ee99f0253459344e48ec32860ed6
Guilherme <132455461+Guilherme-silva-teixeira@users.noreply.github.com>
2024-08-16 20:53:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
-let menu = document.getElementById("menu").addEventListener("load",
-    
+let activeHover = document.getElementById("out");
+
+let service1 = document.getElementById("service1");
+let service2 = document.getElementById("service2");
+let service3 = document.getElementById("service3");
+
+let onePerTen = 77;
+
+let buttonLeft = document.getElementById("left").addEventListener("click",
+    function()
+    {
+        onePerTen = (onePerTen + 77);
+        service1.style.marginLeft = onePerTen + "px";
+        service1.style.transition = "0.5s ease-in-out";
+        service2.style.transition = "0.5s ease-in-out";
+        service3.style.transition = "0.5s ease-in-out";
+    }
+);
+
+
+let buttonRight = document.getElementById("right").addEventListener("click",
+    function()
+    {
+        onePerTen = (onePerTen + 77);
+        service3.style.marginRight = onePerTen + "px";
+    }
 );
