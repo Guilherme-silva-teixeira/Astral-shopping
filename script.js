32a76a4e568ebb1f5b04d581a3343b9fef5762a7
Guilherme <132455461+Guilherme-silva-teixeira@users.noreply.github.com>
2024-08-17 13:09:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,22 @@
-let activeHover = document.getElementById("out");
+let imageContaineer = document.getElementById("imges");
+imageContaineer.style.transition = "0.1s ease";
 
-let service1 = document.getElementById("service1");
-let service2 = document.getElementById("service2");
-let service3 = document.getElementById("service3");
+let swipePos = 177;
+let atualPos = 0;
 
-let onePerTen = 77;
-let minusPerTen = 77;
-
-let buttonLeft = document.getElementById("left").addEventListener("click",
+let left = document.getElementById("left").addEventListener("click",
     function()
     {
-        onePerTen = (onePerTen + 77);
-        minusPerTen = (minusPerTen - 77);
-
-        service1.style.marginLeft = onePerTen + "px";
-        service3.style.marginLeft = minusPerTen + "px";
-        
-        service1.style.transition = "0.5s ease-in-out";
-        service2.style.transition = "0.5s ease-in-out";
-        service3.style.transition = "0.5s ease-in-out";
+        atualPos -= swipePos;
+        imageContaineer.style.marginLeft = atualPos + "px";
+        console.log(imageContaineer.style.marginLeft);
     }
 );
 
-let buttonRight = document.getElementById("right").addEventListener("click",
+let right = document.getElementById("right").addEventListener("click",
     function()
     {
-        onePerTen = (onePerTen + 77);
-        minusPerTen = (minusPerTen - 77);
-
-        service3.style.marginRight = onePerTen + "px";
-        service1.style.marginRight = minusPerTen + "px";
-
-        service1.style.transition = "0.5s ease-in-out";
-        service2.style.transition = "0.5s ease-in-out";
-        service3.style.transition = "0.5s ease-in-out";
+        atualPos += swipePos;
+        imageContaineer.style.marginLeft = atualPos + "px";
     }
-                                                                    
 );
