724b7cc9e08bd63a2948cdc5f9860ee9664a6a6e
Guilherme <132455461+Guilherme-silva-teixeira@users.noreply.github.com>
2024-08-16 21:23:43 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,23 +5,35 @@ let service2 = document.getElementById("service2");
 let service3 = document.getElementById("service3");
 
 let onePerTen = 77;
+let minusPerTen = 77;
 
 let buttonLeft = document.getElementById("left").addEventListener("click",
     function()
     {
         onePerTen = (onePerTen + 77);
+        minusPerTen = (minusPerTen - 77);
+
         service1.style.marginLeft = onePerTen + "px";
+        service3.style.marginLeft = minusPerTen + "px";
+        
         service1.style.transition = "0.5s ease-in-out";
         service2.style.transition = "0.5s ease-in-out";
         service3.style.transition = "0.5s ease-in-out";
     }
 );
 
-
 let buttonRight = document.getElementById("right").addEventListener("click",
     function()
     {
         onePerTen = (onePerTen + 77);
+        minusPerTen = (minusPerTen - 77);
+
         service3.style.marginRight = onePerTen + "px";
+        service1.style.marginRight = minusPerTen + "px";
+
+        service1.style.transition = "0.5s ease-in-out";
+        service2.style.transition = "0.5s ease-in-out";
+        service3.style.transition = "0.5s ease-in-out";
     }
+                                                                    
 );
