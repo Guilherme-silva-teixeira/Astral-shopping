b5bf68f5afd8dc47004db520778a2e65eab15b36
Guilherme <132455461+Guilherme-silva-teixeira@users.noreply.github.com>
2024-08-26 20:26:12 -0300

    Update script.js

--- store/script.js
+++ store/script.js
@@ -37,31 +37,34 @@ let desInp = document.getElementById("des-btn").addEventListener("click",
     function()
     {
         let outContainer = document.getElementById("javascript-popup-container");
-        outContainer.innerHTML =
+        outContainer.innerHTML +=
         `
-        <button id="close">Fechar</button>
            <div id="popup">
-                <div id="window">
+                  <div id="window">
                         <p>Insira o nome do produto</p>
                     <input type="text" id="productName" placeholder="Adicionar produto:">
                         <p>Insira o tipo do produto</p>
                     <input type="text" id="productType" placeholder="Adicionar produto:">
                         <p>Insira o preço do produto</p>
                     <input type="text" id="productPrice" placeholder="Adicionar produto:">
+                    <p>Insira o link de compra</p>
+                    <input type="text" id="productLink" placeholder="Adicionar produto:">
                     <button id="save">Adicionar</button>
+                                <button id="close">cancelar</button>
                 </div>
            </div>
            <style>
                 #window
                 {
-                    height: 251px;
-                    width: 251px;
+                    height: 277px;
+                    width: 277px;
                     background: #fff;
                     color: #000;
                     display: flex;
                     flex-direction: column;
                     justify-content: center;
                     align-items: center;
+                    border-radius: 3px;
                 }
                 #popup
                 {
@@ -80,12 +83,37 @@ let desInp = document.getElementById("des-btn").addEventListener("click",
                         margin-left: 25%;
                     }
                 }*/
+               #window button#save
+                {
+                    height: 27px;
+                    width: 150px;
+                    background: #111;
+                    color: #fff;
+                    border: 0;
+                    border-radius: 3px;
+                    cursor: pointer;
+               }
+            #window input
+            {
+                height: 21px;
+                margin-top: 3px;
+                margin-bottom: 3px;
+                border-radius: 3px;
+                border: 1px solid #000;
+            }
+                button#close
+                {
+                    margin-top: 3px; 
+                    height: 27px;
+                    width: 150px;
+                    background: #f00;
+                    color: #fff;
+                    border: 0;
+                    border-radius: 3px;
+                    cursor: pointer;
+                }
            </style>
         `;
-
-        let productName = document.getElementById("productName").value;
-        let productType = document.getElementById("productType").value;
-        let productPrice = document.getElementById("productPrice").value;
 
         let closeButton = document.getElementById("close").addEventListener("click",
             function()
@@ -98,13 +126,16 @@ let desInp = document.getElementById("des-btn").addEventListener("click",
                 outContainer.style.height = "0px";
                 outContainer.style.width = "0px";
                 outContainer.style.color = "#0000";
-                outContainer.style.position = "absolute";
             }
         );
 
         let save = document.getElementById("save").addEventListener("click",
             function()
-            {
+            {        
+                let productName = document.getElementById("productName").value;
+                let productType = document.getElementById("productType").value;
+                let productPrice = document.getElementById("productPrice").value;
+
                 let firstL = document.getElementById("firstL").innerHTML +=
                 `
                 <div id="product">
