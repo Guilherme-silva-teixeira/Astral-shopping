4504e26d569aee219e748a113d6360e8a2f794e8
Guilherme <132455461+Guilherme-silva-teixeira@users.noreply.github.com>
2024-08-26 21:39:00 -0300

    Update script.js

--- script.js
+++ script.js
@@ -12,9 +12,24 @@ let right = document.getElementById("right").addEventListener("click", function(
     updateBackground();
 });
 
-imageContainer.innerHTML = `<h1>Montamos Seu pc</h1>
-                            <p>A sua máquina é montada pelos melhores profissionais do Pais.</p>`;
+imageContainer.innerHTML = `<h1 id="titl">Montamos Seu pc</h1>
+                            <p id="desc">A sua máquina é montada pelos melhores profissionais do Pais.</p>`;
 
+
+    
+                            imageContainer.style.flexDirection = "column";
+    let titl = document.getElementById("titl");
+    let desc = document.getElementById("desc");
+
+    titl.style.fontFamily = "'poppins',sans-serif";
+    titl.style.fontWeight = 300;
+    titl.style.margin = "7px";
+    titl.style.fontSize = "47px";
+
+    desc.style.fontFamily = "'roboto',sans-serif";
+    desc.style.fontWeight = 300;
+    desc.style.margin = "7px";
+    desc.style.fontSize = "17px";
 /*    <!--<iv id="service1">
                             <h1>Montamos Seu pc</h1>
                             <p>A sua máquina é montada pelos melhores profissionais do Pais.</p>
@@ -30,21 +45,37 @@ imageContainer.innerHTML = `<h1>Montamos Seu pc</h1>
 
 function updateBackground() {
     imageContainer.style.color = "#fff";
+    imageContainer.style.transition = "0.7s ease";
+    imageContainer.style.display = "flex";
+    imageContainer.style.flexDirection = "column";
     if (clicks == 1) {
         imageContainer.style.background = "url('img/as1.png')";
-        imageContainer.innerHTML = `<h1>Montamos Seu pc</h1>
-                            <p>A sua máquina é montada pelos melhores profissionais do Pais.</p>`;
+        imageContainer.innerHTML = `<h1 id="titl">Montamos Seu pc</h1>
+                            <p id="desc">A sua máquina é montada pelos melhores profissionais do Pais.</p>`;
     } else if (clicks == 2) {
         imageContainer.style.background = "url('img/as2.png')";
-        imageContainer.innerHTML = `<h1>Garantia de 2 anos</h1>
-                            <p>Nossos produtos possuem uma garantia de 2 anos com total suporte para defeitos.</p>
+        imageContainer.innerHTML = `<h1 id="titl">Garantia de 2 anos</h1>
+                            <p  id="desc">Nossos produtos possuem uma garantia de 2 anos com total suporte para defeitos.</p>
                         `;
     } else if (clicks == 3) {
         imageContainer.style.background = "url('img/as3.png')";
         imageContainer.innerHTML= `
-                            <h1>Suporte 24 horas</h1>
-                            <p>Nosso atendimento especialixado em manutenção com suporte 24 horas por dia 7 dias por semana.</p>`;
+                            <h1 id="titl">Suporte 24 horas</h1>
+                            <p  id="desc">Nosso atendimento especialixado em manutenção com suporte 24 horas por dia 7 dias por semana.</p>`;
     }
+               
+    let titl = document.getElementById("titl");
+    let desc = document.getElementById("desc");
+
+    titl.style.fontFamily = "'poppins',sans-serif";
+    titl.style.fontWeight = 300;
+    titl.style.margin = "7px";
+    titl.style.fontSize = "47px";
+
+    desc.style.fontFamily = "'roboto',sans-serif";
+    desc.style.fontWeight = 300;
+    desc.style.margin = "7px";
+    desc.style.fontSize = "17px";
 }
 
 
