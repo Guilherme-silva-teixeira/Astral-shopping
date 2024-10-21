c320b3259f74c2740e256b4519deb9a971b5e1ce
Guilherme <132455461+Guilherme-silva-teixeira@users.noreply.github.com>
2024-10-21 18:32:56 -0300

    Update script.js

--- store/script.js
+++ store/script.js
@@ -1,9 +1,10 @@
 let configButton = document.getElementById("configButton").addEventListener("click",
     function()
     {
+        //falta colocar mais elementos para o tema escuro como: header, colocar imagens png, etc.
         let popupArea = document.getElementById("popupArea");
         popupArea.innerHTML = `<div id="popup1">
-            <label id="title">Configurações</label><br><br>
+            <label id="title"><div id="separator">Configurações <button id="closePopup">X</button></div></label><br><br>
             <label>Tema:</label>&emsp;<button id="changeColorTheme" onclick="changeColor()">Alternar</button><br>
         </div>`;
 
@@ -24,15 +25,42 @@ let configButton = document.getElementById("configButton").addEventListener("cli
         popupArea.style.display = "flex";
         popupArea.style.justifyContent = "center";
         popupArea.style.alignItems = "center";
+
+        let separatorDiv = document.getElementById("separator");
+        separatorDiv.style.display = "flex";
+        separatorDiv.style.flexDirection = "row";
+        separatorDiv.style.alignItems = "space-between";
+
+        let buttonClosePopup = document.getElementById("closePopup").addEventListener("click",
+            function()
+            {
+                let popTitle = document.getElementById("title");
+                let popup01 = document.getElementById("popup1");
+                let popupArea1 = document.getElementById("popupArea");
+                popupArea1.innerHTML = ``;
+                popTitle.style.height = 0;
+                popTitle.style.width = 0;
+                popupArea1.style.height = 0;
+                popupArea1.style.width = 0;
+                popupArea1.style.zIndex = -1;
+                popupArea1.style.background = "#0000";
+            }
+        );
     }
 );
 
 function changeColor()
 {
     let body = document.getElementById("body");
-    let afLink = document.getElementById("")
     body.style.color = "#fff";
     body.style.backgroundColor = "#000";
+    let prodTitle = document.getElementById("lin1");
+    let prodPrice = document.querySelectorAll("#lin2");
+    let paymentType = document.querySelectorAll("#lin3");
+    prodPrice.style.color = "#fff";
+    let header01 = document.getElementById("header01");
+    header01.style.color = "#fff";
+    header01.style.background = "#000";
 }
 
 let product = document.getElementById("product").addEventListener("mouseenter",
@@ -48,5 +76,12 @@ let productOnmouseour = document.getElementById("product").addEventListener("mou
     function()
     {
         let mouseEnterArea = document.getElementById("mouseEnterArea").innerHTML = "";
+    }
+);
+
+let product01 = document.getElementsByClassName("product01").addEventListener("mouseenter",
+    function()
+    {
+        this.style.cursor = "pointer";
     }
 );
